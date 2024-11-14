db1d606bde4d59893a2f27aa1ab5c96e2e1012bb
jose150594 <josedelgado1905@gmail.com>
2024-11-13 21:07:47 -0600

    Commit inicial

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
-# Descripción: Este archivo contiene el punto de entrada principal del proyecto.
-# Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
-# Esto ayuda a mantener un registro claro del trabajo realizado.
+# Proyecto: Compra Fruteria
+# Estudiante: Jose Pablo Delgado
+# Fecha de Inicio: 13/11/2024
+# Fecha de Entrega: 13/11/2024
+# Descripción: Comtrolar compras de frutas y verduras
+# Version 0.0
+from analisis_datos import *
+
+#Generar una lista de compras aleatoria y escribir esta lista en un archivo
+lista_compras = generar_lista_compras()
+guardar_lista_compras(lista_compras)
+precios = [precio for _, precio in lista_compras]
+media = media(precios)
+mediana = mediana(precios)
+print(f"Media de precios: ¢{media:.2f}")
+print(f"Mediana de precios: ¢{mediana:.2f}")
